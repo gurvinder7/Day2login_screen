56ec97e8ff69fe939d300130c5efea088866c4c4
gurvindermangat <gurvinder7@yahoo.com>
2019-03-28 19:24:02 -0400

    loging designed and code

--- app/src/main/java/com/example/day2/MainActivity.java
+++ app/src/main/java/com/example/day2/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.day2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtUseremail;
+    private EditText edtPassword;
+    private Button btnLogin;
+    private  Button btnClear;
+    private TextView txtMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        edtUseremail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        txtMsg = findViewById(R.id.lblMessage);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnClear = findViewById(R.id.btnClear);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = edtUseremail.getText().toString();
+                Toast.makeText(MainActivity.this,email,Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(),email,Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
